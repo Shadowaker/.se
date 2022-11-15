c19c46f89ad98cb6f5c10912cb2b8278f3c59148
Dan <41269618+Shadowaker@users.noreply.github.com>
2022-11-15 17:38:54 +0100

    Update sw.sh

--- sw.sh
+++ sw.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 osascript -e \
-'tell application "System Events" to tell appearance preferences to set dark mode to not dark mode'
-
-
+'tell application "System Events" to tell appearance preferences to set dark mode to true'
 defaults -currentHost write -g KeyRepeat -int 2
 defaults -currentHost write -g InitialKeyRepeat -int 15
 defaults -currentHost write -g com.apple.swipescrolldirection -bool FALSE
-
